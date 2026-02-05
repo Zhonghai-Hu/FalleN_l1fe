d30c7ef3bea962123f44f373df026e874440f53f
Zhonghai Hu <hzh337844646@gmail.com>
2026-02-05 15:59:38 -0800

    fix login loop

--- Python/Homework/program_1.py
+++ Python/Homework/program_1.py
@@ -1,20 +1,27 @@
 password = ["root"]
 username = ["root"]
 a = 0
-while a<=3:
+
+while a<3:
+
     input_1 = input("login or create an account: ")
+
     if input_1 == "login":
         found = 0
         u_try = input('please enter your username')
         p_try = input('please enter your password')
+
         for i in range(len(username)):
+
             if username[i] == u_try and password[i] == p_try:
                 print('welcome')
                 found = 1
                 break
+
         if found != 1:
             print("not a valable password or username")
             a+= 1
+
         else:
             break
 
@@ -23,7 +30,7 @@ while a<=3:
         p_try = input('please enter your password')
         password.append(p_try)
         username.append(u_try)
-        a += 1
         continue
+    
     else:
         break
